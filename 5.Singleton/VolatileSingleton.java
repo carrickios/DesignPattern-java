103d62b0bc396ed0eb3f7d36e7d18fcdf297d1e9
Carrick <hourui6@xdf.cn>
2022-11-18 08:59:54 +0800

    add sample code

---
+++ 5.Singleton/VolatileSingleton.java
@@ -0,0 +1,28 @@
+/*
+ * @Author: hourui 
+ * @Date: 2022-11-18 08:57:21 
+ * @Last Modified by: hourui
+ * @Last Modified time: 2022-11-18 08:59:29
+ */
+
+public class VolatileSingleton {
+    // volatile 关键字确保 当uniqueInstance被初始化成VolatileSingleton实例时，多个线程正确处理uniqueInstance变量
+    private volatile static VolatileSingleton uniqueInstance;
+
+    private VolatileSingleton() {};
+
+    public VolatileSingleton getInstance() {
+        if (uniqueInstance == null) {
+            synchronized (VolatileSingleton.class) {
+                if (uniqueInstance == null) {
+                    uniqueInstance = new VolatileSingleton();
+                }
+            }
+        }
+        return uniqueInstance;
+    }
+
+    public void otherMethod() {
+
+    }
+}
